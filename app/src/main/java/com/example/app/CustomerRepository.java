033f62b80fa7bb837a67475af0c6ace8d80d1245
Laura Valentina Garzon Guiza <lvgarzon@bancolombia.com.co>
2024-09-24 18:47:46 -0500

    First commit

---
+++ app/src/main/java/com/example/app/CustomerRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.app;
+
+/**
+ *
+ * @author lvgarzon
+ */
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+  List<Customer> findByLastName(String lastName);
+
+  Customer findById(long id);
+}
